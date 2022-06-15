8b4f6cd97fe920f3ea6a3a69a20bb355f9e8a215
unotanj <jounotan@usm.edu.ph>
2022-06-15 23:41:52 +0800

    renaming

--- RMSapp/views.py
+++ RMSapp/views.py
@@ -7,29 +7,14 @@ from .filters import *
 
 
 def homePage(request):
-    courses_count = Course.objects.all().count()
     seminars_count = Seminar.objects.all().count()
     workshops_count = Workshop.objects.all().count()
-    schedules_count = Schedule.objects.all().count()
 
     context = {
-        'courses_count': courses_count,
         'seminars_count': seminars_count,
         'workshops_count': workshops_count,
-        'schedules_count': schedules_count,
     }
     return render(request, 'home.html', context)
-
-
-def addCourse(request):
-    if request.method == 'POST':
-        course_code = request.POST['course_code']
-        subject_title = request.POST['subject_title']
-        saveCourse = Course(course_code=course_code, subject_title=subject_title)
-        saveCourse.save()
-        messages.info(request, 'Added Successfully!')
-
-    return render(request, 'add_course.html')
 
 
 def addSeminar(request):
@@ -44,8 +29,6 @@ def addSeminar(request):
 
 
 def addWorkshop(request):
-    subjectOptions = Course.objects.all()
-
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -54,33 +37,7 @@ def addWorkshop(request):
         saveWorkshop.save()
         messages.info(request, 'Added Successfully!')
 
-    return render(request, 'add_workshop.html', {'subjectOptions': subjectOptions})
-
-
-def addSchedule(request):
-    subjectOptions = Course.objects.all()
-
-    if request.method == 'POST':
-        date_schedule = request.POST['date_schedule']
-        time_schedule = request.POST['time_schedule']
-        subject = request.POST['subject']
-        saveSchedule = Schedule(date_schedule=date_schedule, time_schedule=time_schedule, subject=subject)
-        saveSchedule.save()
-        messages.info(request, 'Added Successfully!')
-
-    return render(request, 'add_schedule.html', {'subjectOptions': subjectOptions})
-
-
-def viewCourse(request):
-    courses = Course.objects.all()
-    courseFilter = CourseFilter(request.GET, queryset=courses)
-    courses = courseFilter.qs
-
-    context = {
-        'courses': courses,
-        'courseFilter': courseFilter
-    }
-    return render(request, 'view_course.html', context)
+    return render(request, 'add_workshop.html', )
 
 
 def viewSeminar(request):
@@ -107,58 +64,18 @@ def viewWorkshop(request):
     return render(request, 'view_workshop.html', context)
 
 
-def viewSchedule(request):
-    schedules = Schedule.objects.all()
-    scheduleFilter = ScheduleFilter(request.GET, queryset=schedules)
-    schedules = scheduleFilter.qs
-
-    context = {
-        'schedules': schedules,
-        'scheduleFilter': scheduleFilter
-    }
-    return render(request, 'view_schedule.html', context)
-
-
-def editCourse(request, courseId):
-    courseItem = Course.objects.get(id=courseId)
-    return render(request, 'edit_course.html', {'courseItem': courseItem})
-
-
 def editSeminar(request, seminarId):
     seminarItem = Seminar.objects.get(id=seminarId)
     return render(request, 'edit_seminar.html', {'seminarItem': seminarItem})
 
 
 def editWorkshop(request, workshopId):
-    subjectOptions = Course.objects.all()
     workshopItem = Workshop.objects.get(id=workshopId)
 
     context = {
         'workshopItem': workshopItem,
-        'subjectOptions': subjectOptions
     }
     return render(request, 'edit_workshop.html', context)
-
-
-def editSchedule(request, scheduleId):
-    subjectOptions = Course.objects.all()
-    scheduleItem = Schedule.objects.get(id=scheduleId)
-
-    context = {
-        'scheduleItem': scheduleItem,
-        'subjectOptions': subjectOptions,
-    }
-    return render(request, 'edit_schedule.html', context)
-
-
-def updateCourse(request, courseId):
-    courseItem = Course.objects.get(id=courseId)
-    courseItem.course_code = request.POST['course_code']
-    courseItem.subject_title = request.POST['subject_title']
-    courseItem.save()
-    messages.info(request, 'Updated Successfully!')
-
-    return redirect(viewCourse)
 
 
 def updateSeminar(request, seminarId):
@@ -182,25 +99,6 @@ def updateWorkshop(request, workshopId):
     return redirect(viewWorkshop)
 
 
-def updateSchedule(request, scheduleId):
-    scheduleItem = Schedule.objects.get(id=scheduleId)
-    scheduleItem.date_schedule = request.POST['date_schedule']
-    scheduleItem.time_schedule = request.POST['time_schedule']
-    scheduleItem.subject = request.POST['subject']
-    scheduleItem.save()
-    messages.info(request, 'Updated Successfully!')
-
-    return redirect(viewSchedule)
-
-
-def deleteCourse(request, courseId):
-    courseItem = Course.objects.get(id=courseId)
-    courseItem.delete()
-    messages.info(request, 'Deleted Successfully!')
-
-    return redirect(viewCourse)
-
-
 def deleteSeminar(request, seminarId):
     seminarItem = Seminar.objects.get(id=seminarId)
     seminarItem.delete()
@@ -215,11 +113,3 @@ def deleteWorkshop(request, workshopId):
     messages.info(request, 'Deleted Successfully!')
 
     return redirect(viewWorkshop)
-
-
-def deleteSchedule(request, scheduleId):
-    scheduleItem = Schedule.objects.get(id=scheduleId)
-    scheduleItem.delete()
-    messages.info(request, 'Deleted Successfully!')
-
-    return redirect(viewSchedule)
